ecec3099ef0ed163810661daef17bd5f0807391a
Maria Borges <mariaeduardabatistaborges648@gmail.com>
2026-02-19 10:04:44 -0300

    Primeira aula de programação

---
+++ aula01.py
@@ -0,0 +1,9 @@
+idade = 15 # variável inteira
+peso = 56.5 # variável float
+nome = "Maria Eduarda Batista Borges" # variável string 
+sexo_feminino = True # variável bool
+
+print(nome)
+print(idade)
+print(peso)
+print(sexo_feminino)

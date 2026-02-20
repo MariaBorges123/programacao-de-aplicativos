564609292607bc7b5866461441abd8f10b7a72f2
Maria.Borges123 <mariaeduardabatistaborges648@gmail.com>
2026-02-20 09:53:55 -0300

    aula matematica

---
+++ aluno.py
@@ -0,0 +1,9 @@
+aluno = input("Digite seu nome")
+telefone = int(input("Digite seu telefone"))
+idade = int(input("Digite sua idade"))
+cidade = input("Digite sua cidade")
+
+print ("Seu nome é" , aluno)
+print ("Sua telefone é" , telefone)
+print ("Sua idade é" , idade)
+print (" Você mora em" , cidade)
---
+++ matematica.py
@@ -0,0 +1,12 @@
+valor = int(input("digite um valor"))
+valor2 = int(input(" digite o valor2")) 
+
+soma = valor+valor2
+subtracao = valor-valor2
+divisao = valor/valor2
+multiplicacao = valor*valor2
+
+print ("A sua soma é " , soma)
+print ("A sua subtracao é" , subtracao)
+print ("A divisão é" , divisao)
+print ("A multiplicação é" , multiplicacao) 

6c0024e9a2ea4efa237168e92b56d0d544bb0ce4
Maria Borges <mariaeduardabatistaborges648@gmail.com>
2026-05-14 10:01:07 -0300

    14/05

--- planejador.py
+++ planejador.py
@@ -1,31 +1,33 @@
-open('viagens.txt' , 'w').close()
+def criar_arquivo():
+
+    open('viagens.txt' , 'w').close()
 
 def criar():
     adicionar = input("Digite o destino desejado: ")
     with open('viagens.txt' , 'a') as viagem:
-        viagem.write(adicionar + '/n')
+        viagem.write(adicionar + '\n')
     print("Destino da viagem adicionado!")
 
 def ler():
-    with open('viagem.txt', 'r') as viagem:
+    with open('viagens.txt', 'r') as viagem:
         destinos = viagem.readlines()
 
         i=0
         for destino in destinos:
-            print(f"{i} - {destinos.strip()}")
+            print(f"{i} - {destino.strip()}")
             i+=1
 
 def atualizar():
     ler()
-    alterar = input("Digite o seu novo destino: ")
+    idx = int(input("Digite o id do destino que deseja alterar: "))
     novo_destino = input("Novo destino: ")
 
-    with open('viagem.txt' , 'r') as viagem:
+    with open('viagens.txt' , 'r') as viagem:
         linhas = viagem.readlines()
 
-        linhas[alterar] = novo_destino + '/n'
+        linhas[idx] = novo_destino + '\n'
 
-        with open('viagem' , 'w') as viagem:
+        with open('viagens.txt' , 'w') as viagem:
             viagem.writelines(linhas)
         print("Destino atualizado!")
 
@@ -43,7 +45,7 @@ def deletar():
     print("Destino removido!")
 
 while True:
-    print("/n1-Cadastrar | 2-Listar | 3-Editar | 4-Excluir | 5-sair")
+    print("\n1-Cadastrar  \n2-Listar  \n3-Editar  \n4-Excluir  \n5-sair")
     opcao = input("Escolha: ")
 
     if opcao == '1': criar()

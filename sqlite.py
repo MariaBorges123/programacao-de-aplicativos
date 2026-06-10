89b8eeaca3d9240fd9a7ed58d3d5b65b59c71d04
Maria Borges <mariaeduardabatistaborges648@gmail.com>
2026-06-10 12:04:24 -0300

    10/06

---
+++ sqlite.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+conexao = sqlite3.connect('escola_demostracao.db')
+cursor = conexao.cursor()
+
+cursor.execute('''
+                CREATE TABLE IF NOT EXISTS alunos(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome TEXT NOT NULL,
+                telefone TEXT,
+                turma TEXT,
+                idade INTEGER,
+                cpf TEXT UNIQUE NOT NULL
+               )''')
+
+nome_aluno = input("Digite a idade do aluno: ")
+telefone_aluno = input("Digite o telefone do aluno: ")
+turma_aluno = input("Digite a turma: ")
+idade_aluno = int(input("Digite a idade do novo aluno: "))
+cpf_aluno = input("Digite o cpf do aluno: ")
+
+comando_inserir = (f'''INSERT INTO alunos(nome, telefone, turma, idade)
+                     VALUES('{nome_aluno}', '{telefone_aluno}', '{turma_aluno}', {idade_aluno} , '{cpf_aluno}')''')
+

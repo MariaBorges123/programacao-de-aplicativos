2f36ee26834c4b41c69ae939b1695f51221d05c8
Maria Borges <mariaeduardabatistaborges648@gmail.com>
2026-06-11 10:11:25 -0300

    11/06

--- sqlite.py
+++ sqlite.py
@@ -19,6 +19,13 @@ turma_aluno = input("Digite a turma: ")
 idade_aluno = int(input("Digite a idade do novo aluno: "))
 cpf_aluno = input("Digite o cpf do aluno: ")
 
-comando_inserir = (f'''INSERT INTO alunos(nome, telefone, turma, idade)
+comando_inserir = (f'''INSERT INTO alunos(nome, telefone, turma, idade, cpf)
                      VALUES('{nome_aluno}', '{telefone_aluno}', '{turma_aluno}', {idade_aluno} , '{cpf_aluno}')''')
+
+cursor.execute(comando_inserir)
+conexao.commit()
+
+print("Cadastro realizado com sucesso!")
+
+conexao.close()
 

d549e2802cfb8c962a6c2c9813c98c4b436b6eca
maria <mariaeduardabatistaborges648@gmail.com>
2026-09-11 10:10:37 -0300

    11/09

--- desafio/banco.py
+++ desafio/banco.py
@@ -1,44 +1,48 @@
 import sqlite3
 
-def criar_tabela_escola():
-    conexao = sqlite3.connect('desafio/gestao_escolar.db') 
-    cursor = conexao.cursor()  
-    cursor.execute("PRAGMA foreing_keys = ON")
+conexao = sqlite3.connect('banco_escola.db')
+cursor = conexao.cursor()
 
-    cursor.execute('''CREATE TABLE IF NOT EXISTS escolas(
-                        id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        nome_escola TEXT NOT NULL,
-                        cidade TEXT NOT NULL)''')
+def criar_tabelas_escola():
+    
+    try:
+        cursor.execute = ("PRAGMA foreign_keys = ON")
 
-conexao.commit()
-conexao.close()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS escolas (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome_escola TEXT NOT NULL,
+                localização TEXT NOT NULL,
+                capacidade INTEGER NOT NULL
+        )''')
 
-def criar_tabela_turmas():
-    conexao = sqlite3.connect('desafio/gestao_escolar.db') 
-    cursor = conexao.cursor()  
-    cursor.execute("PRAGMA foreing_keys = ON")
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS alunos (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome_aluno TEXT NOT NULL,
+                idade_aluno TEXT NOT NULL,
+                sala_aluno TEXT NOT NULL,
+                id_aluno INTEGER NOT NULL,
+                FOREIGN KEY (id_aluno) REFERENCES escolas(id)
+            )''')
+        
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS professores (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome_professor TEXT NOT NULL.
+                idade_professor TEXT NOT NULL,
+                materia TEXT NOT NULL,
+                sala TEXT NOT NULL,
+                id_professor INTEGER NOT NULL,
+                FOREIGN KEY (id_professor) REFERENCES escolas(id)
+            )''')
+            
+        print("Banco de dados inicializado com sucesso!")
+            
+        conexao.commit()
 
-    cursor.execute('''CREATE TABLE IF NOT EXISTS turmas(
-                        id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        nome_turma TEXT NOT NULL,
-                        foreing keys (id_escola) REFERENCE escolas (id))''')
-
-conexao.commit()
-conexao.close()
-
-def criar_tabela_alunos():
-    conexao = sqlite3.connect('desafio/gestao_escolar.db') 
-    cursor = conexao.cursor()  
-    cursor.execute("PRAGMA foreing_keys = ON")
-
-    cursor.execute('''CREATE TABLE IF NOT EXISTS alunos (
-                        id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        nome_aluno TEXT NOT NULL,
-                        idade INTEGER NOT NULL,
-                        foreing keys (nome_turma) REFERENCE turmas (nome_turma))''')
-
-conexao.commit()
-conexao.close()
-
+    except sqlite3.Error as e:
+        print(f" Erro ao inicializar o banco de dados: {e}")
+        return None
 
 

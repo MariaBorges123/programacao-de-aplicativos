7f861b82d532cbd88e8cb8772e6b72d0eaa3fcaa
maria <mariaeduardabatistaborges648@gmail.com>
2026-09-18 08:49:38 -0300

    18/09

---
+++ matchcasepesquisa.py
@@ -0,0 +1,23 @@
+opcao = int(input("Digite uma opção (1-3): "))
+
+if opcao == 1:
+    print("Cadastrando...")
+elif opcao == 2:
+    print("Listando...")
+elif opcao == 3:
+    print("Saindo...")
+else:
+    print("Opção inválida!")
+
+
+opcao = int(input("Digite uma opção (1-3): "))
+
+match opcao:
+    case 1:
+        print("Cadastrando...")
+    case 2:
+        print("Listando...")
+    case 3:
+        print("Saindo...")
+    case _:
+        print("Opção inválida!")
